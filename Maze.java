c669bf01d6b09054392fd36bb58855865927bee4
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-02-27 09:49:29 -0500

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -1,4 +1,4 @@
-import java.io.File;
+/*import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Arrays;
@@ -23,4 +23,23 @@ public class Maze{
     for(int c = 0; c < ary.length; c++)
     System.out.print(ary[c]);
   }
+}*/
+import java.util.*;
+import java.io.*;
+public class Maze{
+    private char[][]maze;
+    private boolean animate;//false by default
+
+    public Maze(String filename) throws FileNotFoundException{
+        //COMPLETE CONSTRUCTOR
+        animate = false;
+        File text = new File(filename);
+        Scanner s = new Scanner(text);
+        while(s.hasNextLine()){
+          String line = s.nextLine();
+          System.out.println(line);
+        }
+
+    }
 }
+
