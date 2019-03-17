2eb30e0e451d6e81b4515e05e51ce6daef2ba64f
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-17 12:10:49 -0400

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -3,35 +3,44 @@ import java.io.*;
 public class Maze{
     private char[][]maze;
     private boolean animate;//false by default
+    private int rowOfS, colOfS ;
+    private int[][] directions ;
 
-    public Maze(String filename) throws FileNotFoundException{
-        //COMPLETE CONSTRUCTOR
-        animate = false;
-        File text = new File(filename);
-        Scanner s = new Scanner(text);
-        String line = "";
-        while(s.hasNextLine()){
-          line += s.nextLine();
-          line += "\n";
-        }
-        int row = 0;
-        int col = 0;
-        for(int i = 0; i < line.length(); i++){
-          if(line.charAt(i) == '\n'){//to get number of rows
-            row++;
-          }
-        }
-        col = line.length() / row;//to calculate number of cols
-        maze = new char[row][col];
-        int index = 0;//to keep track of the index of line
-        //save the maze into an array
-        for(int r = 0;r <maze.length; r++){
-          for(int c = 0; c < maze[0].length; c++){
-            maze[r][c]= line.charAt(index);
-            index++;
-          }
-        }
+     public Maze(String filename) throws FileNotFoundException {
+    animate = false ;
+    int[][] moves = { {0, -1}, {-1, 0}, {0, 1}, {1, 0} } ;
+    directions = moves ;
+    int len = 0, width = 0 ;
+    File f = new File(filename) ;
+    Scanner s = new Scanner(f) ;
+    String line = "" ;
+    while (s.hasNextLine()) {
+      line = s.nextLine() ;
+      len++ ;
     }
+    width = line.length() ;
+    maze = new char[len][width] ;
+    Scanner adding = new Scanner(f) ;
+    int row = 0, numberOfStarts = 0, numberOfEnds = 0 ;
+    rowOfS = -1 ;
+    colOfS = -1 ;
+    while (adding.hasNextLine() && row < maze.length) {
+      line = adding.nextLine() ;
+      for (int i = 0 ; i < line.length() ; i++) {
+        maze[row][i] = line.charAt(i) ;
+        if (line.charAt(i) == 'S') {
+          rowOfS = row ;
+          colOfS = i ;
+          numberOfStarts+= 1 ;
+        }
+        if (line.charAt(i) == 'E') numberOfEnds+= 1 ;
+      }
+      row++ ;
+    }
+    if (rowOfS == -1 || numberOfStarts != 1 || numberOfEnds != 1) {
+      throw new IllegalStateException("The start and end are either missing, or there's more than one of each!") ;
+    }
+  }
 
 
     private void wait(int millis){
@@ -60,9 +69,44 @@ public class Maze{
       for(int c = 0; c < maze[0].length; c++){
         ans += maze[r][c];
       }
+        ans += "\n" ;
     }
     return ans;
   }
 
+    public int solve() {
+        maze[rowOfS][colOfS] = '@' ;
+        return solveH(rowOfS, colOfS,0) ;
+  }
+    private int solveH(int row, int col, int lengthOfSolution) {
+    if (animate) {
+      clearTerminal() ;
+      System.out.println(this) ;
+      wait(20) ;
+    }
+    if (maze[row][col] == 'E') {
+      setAnimate(false) ;
+      System.out.println(lengthOfSolution) ;
+      return lengthOfSolution ;
+    }
+    int tempR, tempC ;
+    for (int i = 0 ; i < 4 ; i++) {
+      tempR = row + directions[i][0] ;
+      tempC = col + directions[i][1] ;
+      if (tempR >= 0 && tempR <= maze.length - 1 && tempC >= 0
+      && tempC <= maze[0].length - 1 && maze[tempR][tempC] == ' ') {
+        maze[tempR][tempC] = '@' ;
+        solveH(tempR, tempC, lengthOfSolution + 1) ;
+      }
+    }
+    for (int a = 0 ; a < 4 ; a++) {
+      tempR = row + directions[a][0] ;
+      tempC = col + directions[a][1] ;
+      if (maze[tempR][tempC] == 'E') solveH(tempR, tempC, lengthOfSolution) ;
+    }
+    maze[row][col] = '•' ;
+    lengthOfSolution-- ;
+    return -1 ;
+  }
 
 }
